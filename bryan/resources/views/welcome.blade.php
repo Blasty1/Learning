008856ba6ef1f906531fd925f46dbb744bb6d337
Blasty1 <ichigoplayer18@gmail.com>
2019-12-28 22:44:36 +0100

    f

--- bryan/resources/views/welcome.blade.php
+++ bryan/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bryan's site
                 </div>
 
                 <div class="links">
